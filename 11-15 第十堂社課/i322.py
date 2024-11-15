d64c56ca21bc9604c31970d26ba030c22cd0553f
DevBoring <104751875+DevBoring@users.noreply.github.com>
2024-11-15 16:00:58 +0800

    Add files via upload

---
+++ 11-15 第十堂社課/i322.py
@@ -0,0 +1,10 @@
+def hanoi(n, from_pole, to_pole, aux_pole):
+    if n == 1:
+        print(f"Move disk 1 from {from_pole} to {to_pole}")
+        return
+    hanoi(n-1, from_pole, aux_pole, to_pole)
+    print(f"Move disk {n} from {from_pole} to {to_pole}")
+    hanoi(n-1, aux_pole, to_pole, from_pole)
+
+# 測試
+hanoi(int(input()), 'A', 'C', 'B')
