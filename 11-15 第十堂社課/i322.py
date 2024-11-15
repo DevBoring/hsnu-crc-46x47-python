b2694319894d13398f07d49e8134a61821bf9407
DevBoring <104751875+DevBoring@users.noreply.github.com>
2024-11-15 16:03:22 +0800

    Update i322.py

--- 11-15 第十堂社課/i322.py
+++ 11-15 第十堂社課/i322.py
@@ -1,9 +1,9 @@
 def hanoi(n, from_pole, to_pole, aux_pole):
     if n == 1:
-        print(f"Move disk 1 from {from_pole} to {to_pole}")
+        print(f"Move disc 1 from {from_pole} to {to_pole}")
         return
     hanoi(n-1, from_pole, aux_pole, to_pole)
-    print(f"Move disk {n} from {from_pole} to {to_pole}")
+    print(f"Move disc {n} from {from_pole} to {to_pole}")
     hanoi(n-1, aux_pole, to_pole, from_pole)
 
 # 測試
